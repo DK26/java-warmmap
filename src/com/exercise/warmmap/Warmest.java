cf12991a4c2056f77e904e9a1367b159739470c2
David Krasnitsky <dikaveman@gmail.com>
2022-09-18 00:44:57 +0300

    A working version

--- src/com/exercise/warmmap/Warmest.java
+++ src/com/exercise/warmmap/Warmest.java
@@ -3,9 +3,14 @@ package com.exercise.warmmap;
 import java.util.EmptyStackException;
 import java.util.Stack;
 
+// Ideally, I would like to implement either a Swiss hash table map or a B-Tree map,
+// but since it turned to be complicated to learn and to implement in a very limited time frame,
+// I decided to have something working using the same Stack I used to implement the warmest logical flow.
+// Looking back, maybe I should have tried to implement this with my own stack, but I was under the assumption
+// that a B-Tree is required and that it could be the most efficient algorithm I hoped I could easily implement.
+
 public class Warmest<K, V> {
 
-    // TODO: `warmest` is some kind of stack (hidden exercise)
     private final Stack<Entry<K, V>> warmest_stack;
 
     public Warmest() {
@@ -18,14 +23,23 @@ public class Warmest<K, V> {
 
         this.warmest_stack.push(entry);
 
-        // TODO: Assign value to data structure
-
         return this;
+    }
+
+    private Entry<K, V> search_entry(K key) {
+
+        // Inefficient O(n), but working
+        for (Entry<K, V> c : this.warmest_stack) {
+            if (c.key() == key) {
+                return c;
+            }
+        }
+        return null;
     }
 
     public V get(K key) {
 
-        Entry<K, V> found_entry = null; // TODO: Replace `null` with `get_entry()` implementation
+        Entry<K, V> found_entry = search_entry(key);
 
         V result = null;
 
@@ -34,12 +48,11 @@ public class Warmest<K, V> {
             this.warmest_stack.push(found_entry);
         }
 
-        // TODO: Returns either a value or null
         return result;
     }
 
     public V remove(K key) {
-        Entry<K, V> found_entry = null; // TODO: Replace `null` with `get_entry()` implementation
+        Entry<K, V> found_entry = search_entry(key);
 
         if (found_entry == null) {
             return null;
@@ -48,13 +61,11 @@ public class Warmest<K, V> {
         // Remove any trace of the object from the stack
         while(this.warmest_stack.remove(found_entry));
 
-        // TODO: Remove key from map
-
         return found_entry.value();
     }
 
     public V getWarmest() {
-        // Peak in a stack is considered an O(1) operation.
+        // `peek()` for a stack is considered an O(1) operation.
         try {
             return this.warmest_stack.peek().value();
         }catch (EmptyStackException e) {
